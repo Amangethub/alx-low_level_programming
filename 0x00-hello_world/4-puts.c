2df1b39191837aec420405d854eb253bd1d108a4
Amanuel yeneneh <amanuel.yeneneht@gmail.com>
2022-09-08 15:52:54 -0700

    puts last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+
+
 /**
- * main - prints a string
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
+ *
  */
+
 int main(void)
+
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
-	retrun(0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
