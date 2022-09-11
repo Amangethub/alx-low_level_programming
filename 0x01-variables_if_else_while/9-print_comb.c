836bd545d42b269763d154177474c095d0de0dd1
Amanuel yeneneh <amanuel.yeneneht@gmail.com>
2022-09-11 03:49:52 -0700

    number with space and coma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - enetry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{	
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}	
+	putchar('\n');
+	return (0);
+}
