b077ff8a655d886a0e897dbaa884d80b5cb44652
Amanuel yeneneh <amanuel.yeneneht@gmail.com>
2022-09-15 11:45:07 -0700

    multiplie

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ *
+ * @a: input integer
+ * @b: input integer
+ *
+ * Return: the multipled out put
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
