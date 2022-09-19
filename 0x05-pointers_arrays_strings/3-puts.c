2e63e0975736031a43eb53590a12e1da6f9a98b3
Amanuel yeneneh <amanuel.yeneneht@gmail.com>
2022-09-19 06:08:57 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints the string given
+ *
+ * @str: string of char inputed
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		_putchar(str[x]);
+}
