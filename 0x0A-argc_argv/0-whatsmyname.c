d1ca7ac7bbd76d78e185b7418a81aa0dae8091c7
Amanuel yeneneh <amanuel.yeneneht@gmail.com>
2022-09-30 07:27:55 -0700

    arguments to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the function name
+ *
+ * @argc: argument counter
+ * @argv: array of the argument strings
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
